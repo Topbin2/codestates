11a54ce98dcdcf3f62cf2014a59d6bd216f5a809
SANGBIN <mosangbin94@gmail.com>
2022-05-09 12:01:29 +0900

    remove

---
+++ practice/main.js
@@ -0,0 +1,13 @@
+function greetSomeone(firstName) {
+  let time = 'night';
+  if(time === 'night') {
+    let greeting = 'Good Night';
+  }
+
+  return greeting + ' ' + firstName;
+}
+
+greetSomeone('sangbin');
+
+
+
