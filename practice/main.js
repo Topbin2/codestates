ec13632fc7da16e1d242d552f15e3841e3b7433a
MoSangBin <mosangbin94@gmail.com>
2022-05-28 22:36:08 +0900

    promise

--- practice/main.js
+++ practice/main.js
@@ -1,25 +1,29 @@
 class UserStorage {
-  loginUser(id, password, onSuccess, onError) {
-    setTimeout(() => {
-      if (
-        (id === "ellie" && password === "dream") ||
-        (id === "coder" && password === "academy")
-      ) {
-        onSuccess(id);
-      } else {
-        onError(new Error("not found"));
-      }
-    }, 2000);
+  loginUser(id, password) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (
+          (id === "ellie" && password === "dream") ||
+          (id === "coder" && password === "academy")
+        ) {
+          resolve(id);
+        } else {
+          reject(new Error("not found"));
+        }
+      }, 1000);
+    });
   }
 
-  getRoles(user, onSuccess, onError) {
-    setTimeout(() => {
-      if (user === "ellie") {
-        onSuccess({ name: "ellie", role: "admin" });
-      } else {
-        onError(new Error("no access"));
-      }
-    }, 1000);
+  getRoles(user) {
+    return new Promise((resolve, reject) => {
+      setTimeout(() => {
+        if (user === "ellie") {
+          resolve({ name: "ellie", role: "admin" });
+        } else {
+          reject("no access");
+        }
+      }, 1000);
+    });
   }
 }
 
@@ -28,11 +32,67 @@ const userStorage = new UserStorage();
 const id = "coder";
 const password = "academy";
 
-userStorage.loginUser(
-  id,
-  password,
-  (user) => {
-    userStorage.getRoles(user, (obj) => console.log(obj), (error) => console.log(error));
-  },
-  (error) => console.log(error)
-);
+userStorage.loginUser(id, password)
+.then((user) => userStorage.getRoles(user))
+.catch((err) => console.log(err))
+.then((user) => console.log(user))
+.catch((err) => console.log(err))
+
+
+// 코드의 가독성이 너무 떨어짐.
+// 에러가 발생하거나 디버깅을 할 경우, 어려움
+// 유지보수 어려움
+
+// const promise = new Promise((resolve, reject) => {
+//   console.log("doing something");
+
+//   const time = setTimeout(() => {
+//     resolve('ellie');
+//     reject(new Error("no network"));
+//   }, 2000);
+
+// });
+
+// promise
+//   .then((value) => console.log(value))
+//   .catch((err) => console.log(err))
+//   .finally(() => console.log('final'))
+
+// const fetchNumber = new Promise((resolve, reject) => {
+//   setTimeout(() => resolve(1), 1000);
+// })
+
+// fetchNumber
+// .then((num) => num * 2)
+// .then((num) => num * 3)
+// .then((num) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => {
+//       resolve(num - 1);
+//     }, 1000)
+//   })
+// })
+// .then(num => console.log(num))
+
+// const getHen = () => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => resolve(1), 1000);
+//   });
+// };
+
+// const getEgg = (num) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => resolve(num), 1000);
+//   });
+// };
+
+// const cook = (num) => {
+//   return new Promise((resolve, reject) => {
+//     setTimeout(() => resolve(num), 1000);
+//   });
+// };
+
+// getHen() //
+//   .then((num) => getEgg(num))
+//   .then((num) => cook(num))
+//   .then(num => console.log(num));
